b4a730d3ea5edee894fed642773cbe328c633d50
Marcelo Del Fiore <marcelodelfiore@gmail.com>
2019-06-06 16:01:51 -0300

    Created test for Slicing Dice adapter

---
+++ test/unit/test_slicingdice_adapter.py
@@ -0,0 +1,244 @@
+import unittest
+from mock import patch, MagicMock
+
+import dbt.flags as flags
+
+from dbt.adapters.slicingdice import SlicingDiceAdapterAdapter
+import dbt.exceptions
+from dbt.logger import GLOBAL_LOGGER as logger  # noqa
+
+from .utils import config_from_parts_or_dicts, inject_adapter
+
+
+def _sd_conn():
+    conn = MagicMock()
+    conn.get.side_effect = lambda x: 'slicingdice' if x == 'type' else None
+    return conn
+
+
+class BaseTestSlicingDiceAdapter(unittest.TestCase):
+
+    def setUp(self):
+        flags.STRICT_MODE = True
+
+        self.raw_profile = {
+            'outputs': {
+                'service_account': {
+                    'type': 'slicingdice',
+                    'method': 'service-account',
+                    'project': 'dbt-unit-000000',
+                    'schema': 'dummy_schema',
+                    'keyfile': '/tmp/dummy-service-account.json',
+                    'threads': 1,
+                },
+            },
+            'target': 'service_account',
+        }
+
+        self.project_cfg = {
+            'name': 'X',
+            'version': '0.1',
+            'project-root': '/tmp/dbt/does-not-exist',
+            'profile': 'default',
+        }
+
+    def get_adapter(self, target):
+        project = self.project_cfg.copy()
+        profile = self.raw_profile.copy()
+        profile['target'] = target
+
+        config = config_from_parts_or_dicts(
+            project=project,
+            profile=profile,
+        )
+        adapter = SlicingDiceAdapterAdapter(config)
+        inject_adapter(adapter)
+        return adapter
+
+
+class TestSlicingDiceAdapterAcquire(BaseTestSlicingDiceAdapter):
+    @patch('dbt.adapters.bigquery.BigQueryConnectionManager.open', return_value=_bq_conn())
+    def test_acquire_connection_oauth_validations(self, mock_open_connection):
+        adapter = self.get_adapter('service_account')
+        try:
+            connection = adapter.acquire_connection('dummy')
+            self.assertEquals(connection.get('type'), 'slicingdice')
+
+        except dbt.exceptions.ValidationException as e:
+            self.fail('got ValidationException: {}'.format(str(e)))
+
+        except BaseException as e:
+            raise
+
+        mock_open_connection.assert_called_once()
+
+    @patch('dbt.adapters.slicingdice.SlicingDiceConnectionManager.open', return_value=_sd_conn())
+    def test_acquire_connection_service_account_validations(self, mock_open_connection):
+        adapter = self.get_adapter('service_account')
+        try:
+            connection = adapter.acquire_connection('dummy')
+            self.assertEquals(connection.get('type'), 'slicingdice')
+
+        except dbt.exceptions.ValidationException as e:
+            self.fail('got ValidationException: {}'.format(str(e)))
+
+        except BaseException as e:
+            raise
+
+        mock_open_connection.assert_called_once()
+
+    def test_cancel_open_connections_empty(self):
+        adapter = self.get_adapter('service_account')
+        self.assertEqual(adapter.cancel_open_connections(), None)
+
+    def test_cancel_open_connections_master(self):
+        adapter = self.get_adapter('service_account')
+        adapter.connections.thread_connections[0] = object()
+        self.assertEqual(adapter.cancel_open_connections(), None)
+
+    def test_cancel_open_connections_single(self):
+        adapter = self.get_adapter('service_account')
+        adapter.connections.thread_connections.update({
+            0: object(),
+            1: object(),
+        })
+        # actually does nothing
+        self.assertEqual(adapter.cancel_open_connections(), None)
+
+    @patch('dbt.adapters.slicingdice.impl.google.auth.default')
+    @patch('dbt.adapters.slicingdice.impl.google.cloud.slicingdice')
+    def test_location_value(self, mock_sd, mock_auth_default):
+        creds = MagicMock()
+        mock_auth_default.return_value = (creds, MagicMock())
+        adapter = self.get_adapter('loc')
+
+        adapter.acquire_connection('dummy')
+        mock_client = mock_sd.Client
+        mock_client.assert_called_once_with('dbt-unit-000000', creds,
+                                            location='Luna Station')
+
+
+class TestConnectionNamePassthrough(BaseTestSlicingDiceAdapter):
+
+    def setUp(self):
+        super(TestConnectionNamePassthrough, self).setUp()
+        self._conn_patch = patch.object(SlicingDiceAdapterAdapter, 'ConnectionManager')
+        self.conn_manager_cls = self._conn_patch.start()
+
+        self.mock_connection_manager = self.conn_manager_cls.return_value
+        self.conn_manager_cls.TYPE = 'slicingdice'
+
+        self.adapter = self.get_adapter('service_account')
+
+    def tearDown(self):
+        super(TestConnectionNamePassthrough, self).tearDown()
+        self._conn_patch.stop()
+        self._relation_patch.stop()
+
+    def test_get_relation(self):
+        self.adapter.get_relation('db', 'schema', 'my_model')
+        self.mock_connection_manager.get_bq_table.assert_called_once_with('db', 'schema', 'my_model')
+
+    def test_create_schema(self):
+        self.adapter.create_schema('db', 'schema')
+        self.mock_connection_manager.create_dataset.assert_called_once_with('db', 'schema')
+
+    @patch.object(SlicingDiceAdapterAdapter, 'check_schema_exists')
+    def test_drop_schema(self, mock_check_schema):
+        mock_check_schema.return_value = True
+        self.adapter.drop_schema('db', 'schema')
+        self.mock_connection_manager.drop_dataset.assert_called_once_with('db', 'schema')
+
+    def test_get_columns_in_relation(self):
+        self.mock_connection_manager.get_bq_table.side_effect = ValueError
+        self.adapter.get_columns_in_relation(
+            MagicMock(database='db', schema='schema', table_name='ident'),
+        )
+        self.mock_connection_manager.get_sd_table.assert_called_once_with(
+            database='db', schema='schema', identifier='ident'
+        )
+
+
+class TestSlicingDiceRelation(unittest.TestCase):
+    def setUp(self):
+        flags.STRICT_MODE = True
+
+    def test_view_temp_relation(self):
+        kwargs = {
+            'type': None,
+            'path': {
+                'database': 'test-project',
+                'schema': 'test_schema',
+                'identifier': 'my_view'
+            },
+            'table_name': 'my_view__dbt_tmp',
+            'quote_policy': {
+                'identifier': False
+            }
+        }
+        SlicingDiceQueryRelation(**kwargs)
+
+    def test_view_relation(self):
+        kwargs = {
+            'type': 'view',
+            'path': {
+                'database': 'test-project',
+                'schema': 'test_schema',
+                'identifier': 'my_view'
+            },
+            'table_name': 'my_view',
+            'quote_policy': {
+                'identifier': True,
+                'schema': True
+            }
+        }
+        BigQueryRelation(**kwargs)
+
+    def test_table_relation(self):
+        kwargs = {
+            'type': 'table',
+            'path': {
+                'database': 'test-project',
+                'schema': 'test_schema',
+                'identifier': 'generic_table'
+            },
+            'table_name': 'generic_table',
+            'quote_policy': {
+                'identifier': True,
+                'schema': True
+            }
+        }
+        BigQueryRelation(**kwargs)
+
+    def test_external_source_relation(self):
+        kwargs = {
+            'type': 'external',
+            'path': {
+                'database': 'test-project',
+                'schema': 'test_schema',
+                'identifier': 'sheet'
+            },
+            'table_name': 'sheet',
+            'quote_policy': {
+                'identifier': True,
+                'schema': True
+            }
+        }
+        BigQueryRelation(**kwargs)
+
+    def test_invalid_relation(self):
+        kwargs = {
+            'type': 'invalid-type',
+            'path': {
+                'database': 'test-project',
+                'schema': 'test_schema',
+                'identifier': 'my_invalid_id'
+            },
+            'table_name': 'my_invalid_id',
+            'quote_policy': {
+                'identifier': False,
+                'schema': True
+            }
+        }
+        with self.assertRaises(dbt.exceptions.ValidationException):
+            BigQueryRelation(**kwargs)
